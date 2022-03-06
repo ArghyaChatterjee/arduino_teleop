cd0049b1da7805effc6d454596d78dd80b706507
Arghya Chatterjee <arghyame20buet@gmail.com>
2022-03-06 23:24:03 +0600

    updated and deleted some bug

--- teleop/teleop.ino
+++ teleop/teleop.ino
@@ -5,22 +5,21 @@
 
 // BEGIN CONSTS
 //TODO: MAKE SURE THIS ADDRESS MATCHES THAT OF THE CONTROLLER
-const uint8_t ROBOCLAW0 = 0x80;
-const uint8_t ROBOCLAW1 = 0x81;
-const bool REVERSE0 = false;
-const bool REVERSE1 = true;
+const uint8_t ROBOCLAW = 0x80;
+//const bool REVERSE0 = false;
+//const bool REVERSE1 = true;
 const long ROBOCLAW_BAUD_RATE = 38400;
 const long ROBOCLAW_TIMEOUT = 10000; 
 const long CONTROL_TIMEOUT = 1000; //ms to wait  before killing motors
-const float PPRREV = 1440; // encoder pulses per rotation
-const float HALFTRACK = 0.3048; // Robot thiccness/2 in METERS
+const float PPREV = 1440; // encoder pulses per rotation
+const float TRACK = 0.3048; // Robot thiccness in METERS
 const float DIAMETER = 0.3302; // in METERS
 const float REVPM = 1.0 / (M_PI * DIAMETER); // revolutions per meter
 const float PPM = PPREV * REVPM; // encoder pulses per meter
 // END CONSTS
 
 // Use uno's Serial1 (same as sabertooth)
-RoboClaw roboclaw(&Serial1,ROBOCLAW_TIMEOUT);
+RoboClaw roboclaw(&Serial,ROBOCLAW_TIMEOUT);
 unsigned long lastData = 0;
 
 void cmdVelCallback(const geometry_msgs::Twist&);
@@ -43,11 +42,10 @@ void cmdVelCallback(const geometry_msgs::Twist &twist) {
   // [1  t][wz] = [vr]
   const float vLeft = linear - spin * TRACK;
   const float vRight = linear + spin * TRACK;
-  vLeft = REVERSE0 ? -vLeft : vLeft;
-  vRight = REVERSE1 ? -vRight : vRight;
+  //vLeft = REVERSE0 ? -vLeft : vLeft;
+  //vRight = REVERSE1 ? -vRight : vRight;
   
-  roboclaw.SpeedM1M2(ROBOCLAW0, vLeft * PPM, vLeft * PPM);
-  roboclaw.SpeedM1M2(ROBOCLAW1, vRight * PPM, vRight * PPM);
+  roboclaw.SpeedM1M2(ROBOCLAW, vLeft * PPM, vRight * PPM);
 }
 
 void setup() {
@@ -60,7 +58,6 @@ void setup() {
 void loop(){
   handle.spinOnce();
   if(millis() - lastData >= CONTROL_TIMEOUT){
-    roboclaw.SpeedM1M2(ROBOCLAW0, 0, 0);
-    roboclaw.SpeedM1M2(ROBOCLAW1, 0, 0);
+    roboclaw.SpeedM1M2(ROBOCLAW, 0, 0);
   }
 }
